20eca46bd0678de91059ce97c19408fb463390a1
25SCL01-SergioCalderon <scalderondb25@gmail.com>
2026-04-23 23:50:25 -0600

    Add files via upload

---
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        jfrmCalc2 holapos = new jfrmCalc2();
+        holapos.setVisible(true);
+    }
+}
